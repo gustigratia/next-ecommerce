4aa3a7cb03fdc1b81040fa54c43124e207834c1b
delpiera <gustigratia706@gmail.com>
2026-05-09 10:03:12 +0700

    fix: product detail page server component fetch API

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     env: {
-        DB_URI: "mongodb+srv://divyagaurav1602045:PwR2GUxnJWMJo7vS@cluster0.g6en0r5.mongodb.net/?retryWrites=true&w=majority",
-        API_URL: "https://next-ecommerce-bay.vercel.app/"
+        // DB_URI: "mongodb+srv://divyagaurav1602045:PwR2GUxnJWMJo7vS@cluster0.g6en0r5.mongodb.net/?retryWrites=true&w=majority",
+        DB_URI: "mongodb+srv://gustigratia:delpiera06@cluster0.kuaeyei.mongodb.net/ecommerce?appName=Cluster0",
+        API_URL: "http://localhost:3000"
     },
 }
 
