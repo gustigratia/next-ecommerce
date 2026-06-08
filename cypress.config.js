7a94bbb284ef0bbc845aeaeb267c6c201d91a0d8
delpiera <gustigratia706@gmail.com>
2026-06-08 15:26:51 +0700

    chore: update workflow

--- cypress.config.js
+++ cypress.config.js
@@ -2,23 +2,19 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:3000',
+    baseUrl: process.env.CYPRESS_BASE_URL || 'http://localhost:3000',
     specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
     supportFile: 'cypress/support/e2e.js',
-
     setupNodeEvents(on, config) {
       return config;
     },
   },
-
   viewportWidth: 1280,
   viewportHeight: 720,
-
   retries: {
     runMode: 2,
     openMode: 0,
   },
-
   video: true,
   screenshotOnRunFailure: true,
 });
